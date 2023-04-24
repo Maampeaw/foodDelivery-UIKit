eb5b1a62a14b72435cbe56f38a358901f471d112
MarkAmoah <markamoah97@gmail.com>
2023-04-24 00:59:08 +0000

    initial project setup

---
+++ FoodDelivery-UIKit/FoodDelivery-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery-UIKit
+//
+//  Created by Mark Amoah on 4/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
